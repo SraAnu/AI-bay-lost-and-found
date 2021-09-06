c295d1f4c2c0a4eebcd72ecb7c8919d5177c44cf
SraAnu <87373173+SraAnu@users.noreply.github.com>
2021-09-06 10:53:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+objects = []
+status = ""
+song = ""
+
+function preload() {
+    song = loadSound("TakiTaki.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(380, 380)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(380, 380)
+    video.hide()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "status : detecting Objects"
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380)
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status : object detected";
+            fill("#FF0000");
+            percentage = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill()
+            stroke("#FF0000")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (objects[i].label == "person") {
+                document.getElementById("number_of_objects").innerHTML = "Baby found  "
+                song.stop()
+            } else {
+                document.getElementById("number_of_objects").innerHTML = "BABY NOT FOUND !  "
+                song.play()
+            }
+        }
+    }
+}
